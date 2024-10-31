bbc4e7e279d1519cc1e8f292b963f77370de0ea1
sherri <sheharyarajmal02@gmail.com>
2024-10-31 14:20:38 +0500

    CI workflow - bug fix 12

---
+++ backend/eslint.config.js
@@ -0,0 +1,24 @@
+import { ESLint } from "eslint";
+
+export default new ESLint({
+  baseConfig: {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true,
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: "module",
+    },
+    plugins: ["@typescript-eslint"],
+    rules: {
+      // Add your custom rules here
+    },
+  },
+});
